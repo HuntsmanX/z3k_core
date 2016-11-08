c79dfe54265af9007c0558d6417b2a1d454eee7c
Bogdan <bogdanparshencev@gmail.com>
2016-11-08 17:33:00 +0200

    Init models

--- app/models/forms/response.rb
+++ app/models/forms/response.rb
@@ -1,2 +1,6 @@
 class Forms::Response < ApplicationRecord
+  has_many   :sections, class_name: 'Forms::Response::Section', inverse_of: :response, dependent: :destroy
+  belongs_to :testee
+
+  validates :name, presence: true
 end
