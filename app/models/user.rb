24502b947bcc1cf0f6378d5f365475d3b47193c0
Dmitry Pasechnik <dmitriy.pasechnik@zone3000.net>
2016-11-08 15:59:39 +0200

    some cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,38 +6,43 @@ class User < ApplicationRecord
 
   alias_method :authenticate, :valid_password?
 
-  def self.from_token_request(request)
-    email = request.params['auth'] && request.params['auth']['email']
-    user = self.find_by email: email
 
-    unless user
-      user_from_staff = auth_on_staff(request.params['auth'])
-      user = User.create(user_from_staff.merge(password: request.params['auth']['password'])) if user_from_staff.present?
+  class << self
+
+    def from_token_request(request)
+      email = request.params['auth'] && request.params['auth']['email']
+      user = find_by email: email
+
+      unless user
+        user_from_staff = auth_on_staff(request.params['auth'])
+        user = User.create(user_from_staff.merge(password: request.params['auth']['password'])) if user_from_staff.present?
+      end
+
+      user
     end
 
-    user
-  end
-
-  def self.from_token_payload(payload)
-    self.find payload['sub']
-  end
-
-  def self.auth_on_staff(params)
-    staff_credentials = Rails.application.secrets.staff
-    params.merge!(auth_token: staff_credentials['api_key'])
-    response = nil
-
-    RestClient.get(staff_credentials['url'] + '/api/auth', { params: params } ) do |r|
-     response = (r.code == 200 ? JSON.parse(r.body) : nil)
+    def from_token_payload(payload)
+      find payload['sub']
     end
 
-    decorate_response(response) if response
+    def auth_on_staff(params)
+      staff_credentials = Rails.application.secrets.staff
+      params.merge!(auth_token: staff_credentials['api_key'])
+      response = nil
+
+      RestClient.get(staff_credentials['url'] + '/api/auth', {params: params}) do |r|
+        response = (r.code == 200 ? JSON.parse(r.body) : nil)
+      end
+
+      decorate_response(response) if response
+    end
+
   end
 
   private
 
   def self.decorate_response(user_from_staff)
-    user_from_staff.keep_if { |k,v| (User.column_names - ['id']).index k}
+    user_from_staff.keep_if { |k, v| (User.column_names - ['id']).index k }
   end
 
 end
