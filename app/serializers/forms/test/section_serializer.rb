305c04ed01a47a81d9b6893d3dced4db9a3c06d9
Stanislav Tanko <stas.tanko@gmail.com>
2016-11-09 14:35:33 +0200

    tweak tests serializer

--- app/serializers/forms/test/section_serializer.rb
+++
@@ -1,5 +0,0 @@
-class Forms::Test::SectionSerializer < ActiveModel::Serializer
-  attributes :title
-
-  belongs_to :test, serializer: Forms::TestSerializer
-end
--- app/serializers/forms/test_serializer.rb
+++ app/serializers/forms/test_serializer.rb
@@ -1,5 +1,5 @@
 class Forms::TestSerializer < ActiveModel::Serializer
   attributes :name, :created_at
 
-  has_many :sections, serializer: Forms::Test::SectionSerializer
+  has_many :sections
 end
