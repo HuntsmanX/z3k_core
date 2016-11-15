69541dc6219bd452221a47457bcccfb8b773ba7f
Dmitry Pasechnik <dmitriy.pasechnik@zone3000.net>
2016-11-15 18:57:30 +0200

    removed extra arguments from serializers, added test associations to tests->index

--- app/serializers/forms/response/option_serializer.rb
+++ app/serializers/forms/response/option_serializer.rb
@@ -1,5 +1,5 @@
 class Forms::Response::OptionSerializer < ApplicationSerializer
-  attributes :content, :is_correct, :field_id, :order_index, :forms_test_field_id
+  attributes :content, :is_correct, :field_id, :order_index, :user_selected
 
   belongs_to :field
 end
