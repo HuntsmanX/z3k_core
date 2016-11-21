0d146106bee2f9df8779cd54c4ad72cd8a01b408
Bogdan <bogdanparshencev@gmail.com>
2016-11-21 12:01:26 +0200

    Fix serializer

--- app/serializers/forms/response/question_serializer.rb
+++ app/serializers/forms/response/question_serializer.rb
@@ -1,5 +1,5 @@
 class Forms::Response::QuestionSerializer < ApplicationSerializer
-  attributes :content, :order_index, :section_id, :forms_test_section_id
+  attributes :content, :order_index, :section_id
 
   belongs_to :section
   has_many   :fields
