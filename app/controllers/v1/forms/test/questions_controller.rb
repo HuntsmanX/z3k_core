80d05ae2fb50682eb70e3562006bff5fcbe80c5a
Stanislav Tanko <stas.tanko@gmail.com>
2016-11-22 15:59:37 +0200

    minor updates

--- app/controllers/v1/forms/test/questions_controller.rb
+++ app/controllers/v1/forms/test/questions_controller.rb
@@ -1,20 +1,33 @@
 class V1::Forms::Test::QuestionsController < ApplicationController
 
   def create
-    question = Forms::Test::Question.new question_params
-    question.save
-    respond_with question
+    question = ::Forms::Test::Question.new question_params
+    if question.save
+      render json: question, include: [fields: :options]
+    else
+      render json: question.errors.messages, status: 422
+    end
   end
 
   def update
-    question = Forms::Test::Question.find params[:id]
-    question.update_attributes question_params
-    respond_with question
+    question = ::Forms::Test::Question.find params[:id]
+    if question.update_attributes question_params
+      render json: question, include: [fields: :options]
+    else
+      render json: question.errors.messages, status: 422
+    end
   end
 
   def destroy
-    question = Forms::Test::Question.find params[:id]
+    question = ::Forms::Test::Question.find params[:id]
     question.destroy
+    render json: {}
+  end
+
+  def reorder
+    params[:order].each do |id, index|
+      ::Forms::Test::Question.find(id).update_attribute :order_index, index
+    end
     render json: {}
   end
 
@@ -22,11 +35,11 @@ class V1::Forms::Test::QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(
-      :section_id, :content,
+      :section_id, :content, :order_index,
       fields_attributes: [
         :id, :field_type, :block_key, :content, :score, :autocheck, :_destroy,
         options_attributes: [
-          :id, :content, :is_correct, :_destroy
+          :id, :content, :is_correct, :order_index, :_destroy
         ]
       ]
     )
