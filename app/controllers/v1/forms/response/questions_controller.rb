0b6ed98b099f459fba69b4f6c9da4cf9432dee5c
Dmitry Pasechnik <dmitriy.pasechnik@zone3000.net>
2016-11-29 12:21:44 +0200

    added update action to Response::QuestionController

--- app/controllers/v1/forms/response/questions_controller.rb
+++ app/controllers/v1/forms/response/questions_controller.rb
@@ -1,2 +1,27 @@
 class V1::Forms::Response::QuestionsController < ApplicationController
+
+  def update
+    question = ::Forms::Response::Question.find params[:id]
+    if question.update_attributes question_params
+      render json: question, include: [fields: :options]
+    else
+      render json: question.errors.messages, status: 422
+    end
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(
+        :section_id,
+        :order_index,
+        fields_attributes: [
+            :id,
+            :score,
+            :user_score,
+            :_destroy
+        ]
+    )
+  end
 end
