a776133c7d8f3a9d4a866859521fb98b472ee43f
Dmitry Pasechnik <dmitriy.pasechnik@zone3000.net>
2016-11-30 14:52:59 +0200

    merged with latest dev

--- app/serializers/forms/response/option_serializer.rb
+++ app/serializers/forms/response/option_serializer.rb
@@ -1,5 +1,7 @@
 class Forms::Response::OptionSerializer < ApplicationSerializer
-  attributes :content, :is_correct, :field_id, :order_index, :user_selected, :id
+  attributes :field_id, :order_index, :user_selected, :id
+  attribute :content, unless: :testee?
+  attribute :is_correct, unless: :testee?
 
   belongs_to :field
 end
