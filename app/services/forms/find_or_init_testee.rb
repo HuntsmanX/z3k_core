1de9036c4561fcfa58d51e80f5b8fe9f1a1eef05
Bogdan <bogdanparshencev@gmail.com>
2016-12-08 13:44:10 +0200

    Add custom runsacker

--- app/services/forms/find_or_init_testee.rb
+++ app/services/forms/find_or_init_testee.rb
@@ -17,19 +17,21 @@ class Forms::FindOrInitTestee
       t.last_name  = last_name
       t.city_id    = city_id
       t.password   = SecureRandom.hex(4)
-      # t.phone = phone
     end
   end
 
   def get_recruitment
     testee = OpenStruct.new Forms::Testee.show(@params, 'recruitment')
-    User.find_or_initialize_by(recruitment_id: @params) do |t|
+    user = User.find_or_initialize_by(recruitment_id: @params) do |t|
       t.city_id    = testee.city_id
       t.email      = testee.email
       t.first_name = testee.full_name.split&.[](0)
       t.last_name  = testee.full_name.split&.[](1)
       t.password   = SecureRandom.hex(4)
     end
+    user.skip_confirmation!
+    user.save!
+    user
   end
 
   def method_missing name, *args, &block
