8d7e0c118e928ea32969525023ded67e34ae68e5
Bogdan <bogdanparshencev@gmail.com>
2016-12-09 17:08:13 +0200

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,12 +94,7 @@ class User < ApplicationRecord
 
     end
   end
-
-  def confirmation_period_valid?
-    return true
-  end
-
-
+  
   def self.find_or_create_user(response)
     user = User.find_or_initialize_by(email: response['email']) do |user|
       user.password                = response['password']
