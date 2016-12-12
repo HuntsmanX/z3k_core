b490ea5b7fb1b2a443af3d1a003497b6ea24e941
Bogdan <bogdanparshencev@gmail.com>
2016-12-12 20:05:19 +0200

    Add aditional fields to response section serializer

--- app/serializers/forms/response/section_serializer.rb
+++ app/serializers/forms/response/section_serializer.rb
@@ -1,7 +1,7 @@
 class Forms::Response::SectionSerializer < ApplicationSerializer
   attributes :uid, :title, :description, :time_limit, :required_score, :acceptable_score,
              :order_index, :bonus_time, :id, :acceptable_score_unit, :required_score_unit,
-             :user_score, :passed
+             :user_score, :is_passed
 
   has_many   :questions, if: :include_nested?
 
@@ -13,7 +13,7 @@ class Forms::Response::SectionSerializer < ApplicationSerializer
     object&.fields&.sum('user_score')
   end
 
-  def passed
+  def is_passed
     user_scores >= object.required_score ? 'Yes' : 'No'
   end
 
