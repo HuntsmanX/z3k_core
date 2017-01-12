358b7e549043f0f24cfbb032f697a58fbc4eea09
Bogdan <bogdanparshencev@gmail.com>
2017-01-12 11:18:39 +0200

    Refactor

--- app/forms/forms/create_response_form.rb
+++ app/forms/forms/create_response_form.rb
@@ -23,12 +23,20 @@ class Forms::CreateResponseForm
   def persist
     testee_result = ::Forms::FindOrInitTestee.new(@user_id).call
 		if testee_result.success.present?
-			result = ::Forms::DuplicateTestForResponse.new(testee_result.result, test_id)
-			result.call.success.present? ? @response = result.response : self.errors.add(:response, result.result)
+			create_response = ::Forms::DuplicateTestForResponse.new(testee_result.result, test_id)
+			duplicate_test_result(create_response)
 		else
 			self.errors.add(:testee, testee_result.result)
 		end
 		self.errors.any? ? false : @response
   end
+
+	def duplicate_test_result(create_response)
+		if create_response.call.success.present?
+			@response = create_response.response
+		else
+			self.errors.add(:response, create_response.result)
+		end
+	end
 
 end
